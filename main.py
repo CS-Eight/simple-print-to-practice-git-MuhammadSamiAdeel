1214ea2d29bf8803a605b02732e3064454056fff
Muhammad Sami Adeel <Madeel@paedu.org>
2025-12-04 19:57:15 +0000

    Changed the print statement

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-# Welcome to Python!
-# This is your first Python program
 
-# The print() function displays text on the screen
-print("Hello, World!")
-
-# Try printing your own message below:
-# print("Your message here")
+print("Hello Pinnacle")
